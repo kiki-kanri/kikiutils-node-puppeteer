47daa404f64801965bffd1d0f86d7b8ab728da5f
kiki-kanri <a470666@gmail.com>
2023-05-23 18:16:34 +0800

    Edit init browser args and configs.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { executablePath, PuppeteerLaunchOptions} from 'puppeteer';
+import { executablePath, PuppeteerLaunchOptions } from 'puppeteer';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
@@ -6,24 +6,27 @@ import PuppeteerBrowser from './browser';
 
 puppeteer.use(StealthPlugin());
 
-export const getPuppeteerBrowser = async (
-	extraOptions: PuppeteerLaunchOptions = {},
-	replaceArgs: boolean = false
-) => {
+export const getPuppeteerBrowser = async (extraOptions: PuppeteerLaunchOptions = {}, replaceArgs: boolean = false) => {
 	const options: PuppeteerLaunchOptions = {
 		args: [
+			'--arc-disable-app-sync',
 			'--autoplay-policy=no-user-gesture-required',
-			'--disable-setuid-sandbox',
+			'--disable-background-networking',
 			'--disable-blink-features=AutomationControlled',
+			'--disable-default-apps',
+			'--disable-dev-shm-usage',
 			'--disable-extensions',
 			'--disable-gpu',
-			'--disable-infobars',
-			'--ignore-certifcate-errors',
-			'--ignore-certifcate-errors-spki-list',
+			'--disable-setuid-sandbox',
+			'--disable-software-rasterizer',
+			'--disable-stack-profiler',
+			'--metrics-recording-only',
+			'--no-default-browser-check',
+			'--no-first-run',
 			'--no-sandbox',
-			'--start-maximized',
-			'--use-fake-ui-for-media-stream',
-			'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+			'--no-startup-window',
+			'--no-zygote',
+			'--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 		],
 		defaultViewport: null,
 		dumpio: false,
@@ -31,7 +34,8 @@ export const getPuppeteerBrowser = async (
 		headless: false,
 		ignoreDefaultArgs: false,
 		ignoreHTTPSErrors: true,
-		userDataDir: './.cache/pyppeteer_browsers/main'
+		userDataDir: './.cache/pyppeteer_browsers/main',
+		waitForInitialPage: false
 	};
 
 	if (!replaceArgs) {
